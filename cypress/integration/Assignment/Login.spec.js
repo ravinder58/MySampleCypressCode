d8e729ea9cbe3ecba5c16e999619d662d7f119f3
RAVINDER Singh <singhravinder868@gmail.com>
2023-04-01 09:59:00 -0400

    First commit of duplicate folder

--- cypress/integration/Assignment/Login.spec.js
+++ cypress/integration/Assignment/Login.spec.js
@@ -1,11 +1,10 @@
 /// <reference types="Cypress" />
  
-describe('LoginDemotestcase, () => {
+describe('LoginDemonewtestcase, () => {
 
     it('AutomationPractice-Login', () => {
       cy.visit('https://automationteststore.com/')
       cy.contains('Login or register').click()
-       // Implicit assertion using should
        cy.get('#loginFrm_loginname').type('singhravinder')
        // Chaining the assertion using and
        cy.get('#loginFrm_password').type('Pathankot@584')
